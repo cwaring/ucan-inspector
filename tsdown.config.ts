6f6662547d8ab7ec94714a182f4f6413ac9aeb52
Chris Waring <106938+cwaring@users.noreply.github.com>
2026-01-17 00:22:36 +0000

    chore: update deps and exports

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   entry: [
     'src/index.ts',
   ],
-  fixedExtension: true,
+  exports: true,
   alias: {
     '@': fileURLToPath(new URL('./src', import.meta.url)),
   },
